1d2029471c58e248c3534bfc2700fc6d013e1382
Martin <Martin@192.168.58.1>
2016-05-03 11:42:50 +0200

    Masse forskelligt

--- Projekt/src/connector01917/Connector.java
+++ Projekt/src/connector01917/Connector.java
@@ -27,46 +27,47 @@ public class Connector
 	 */
 	public static Connection connectToDatabase(String url, String username, String password)
 			throws InstantiationException, IllegalAccessException,
-					ClassNotFoundException, SQLException
+			ClassNotFoundException, SQLException
 	{
 		// call the driver class' no argument constructor
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		
+
 		// get Connection-object via DriverManager
 		return (Connection) DriverManager.getConnection(url, username, password);
 	}
-	
+
 	private static Connection conn;
 	private static Statement stm;
-	
+
 	public Connector(String server, int port, String database,
 			String username, String password)
-				throws InstantiationException, IllegalAccessException,
+					throws InstantiationException, IllegalAccessException,
 					ClassNotFoundException, SQLException
 	{
 		conn	= connectToDatabase("jdbc:mysql://"+server+":"+port+"/"+database,
-					username, password);
-		stm		= conn.createStatement();
+				username, password);
 	}
-	
+
 	public Connector() throws InstantiationException, IllegalAccessException,
-			ClassNotFoundException, SQLException
+	ClassNotFoundException, SQLException
 	{
 		this(Constant.server, Constant.port, Constant.database,
 				Constant.username, Constant.password);
 	}
-	
+
 	public static ResultSet doQuery(String cmd) throws DALException
 	{
-		try { return stm.executeQuery(cmd); }
+		try {stm = conn.createStatement(); 
+		return stm.executeQuery(cmd); }
 		catch (SQLException e) { throw new DALException(e); }
 	}
-	
+
 	public static int doUpdate(String cmd) throws DALException
 	{
-		try { return stm.executeUpdate(cmd); }
+		try {stm = conn.createStatement();
+		return stm.executeUpdate(cmd); }
 		catch (SQLException e) { throw new DALException(e); }
 	}
-	
-	
+
+
 }
--- Projekt/src/test01917/operatoerTest.java
+++ Projekt/src/test01917/operatoerTest.java
@@ -15,47 +15,56 @@ public class operatoerTest {
 		catch (IllegalAccessException e) { e.printStackTrace(); }
 		catch (ClassNotFoundException e) { e.printStackTrace(); }
 		catch (SQLException e) { e.printStackTrace(); }
-		
+
 		System.out.println("Operatoer nummer 3:");
 		OperatoerImpl opr = new OperatoerImpl();
 		try { System.out.println(opr.getOperatoer(3)); }
 		catch (DALException e) { System.out.println(e.getMessage()); }
-		
+
 		System.out.println("Indsaettelse af ny operatoer med opr_id =  4");
 		OperatoerDTO oprDTO = new OperatoerDTO(4,"Don Juan","DJ","000000-0000","iloveyou");
 		try { opr.createOperatoer(oprDTO); }
 		catch (DALException e) { System.out.println(e.getMessage()); }	
-		
+
 		System.out.println("Operatoer nummer 4:");
 		try { System.out.println(opr.getOperatoer(4)); }
 		catch (DALException e) { System.out.println(e.getMessage()); }
-		
+
 		System.out.println("Opdatering af initialer for operatoer nummer 4");
 		oprDTO.setIni("DoJu");
 		try { opr.updateOperatoer(oprDTO); }
 		catch (DALException e) { System.out.println(e.getMessage()); }
-		
+
 		System.out.println("Operatoer nummer 4:");
 		try { System.out.println(opr.getOperatoer(4)); }
 		catch (DALException e) { System.out.println(e.getMessage()); }
-		
+
 		System.out.println("Alle operatoerer:");
 		try { System.out.println(opr.getOperatoerList()); }
 		catch (DALException e) { System.out.println(e.getMessage()); }
-		
+
 		System.out.println("Indsaettelse af ny operatoer med opr_id =  5");
 		OperatoerDTO oprDTO1 = new OperatoerDTO(5,"Martin","MR","130193-0000","Hejasverige");
 		try { opr.createOperatoer(oprDTO1); }
 		catch (DALException e) { System.out.println(e.getMessage()); }	
-		
+
 		System.out.println("Operatoer nummer 5:");
 		try { System.out.println(opr.getOperatoer(5)); }
 		catch (DALException e) { System.out.println(e.getMessage()); }		
-		
+
 		System.out.println("Update Operator 5");
 		oprDTO1.setCpr("920349-2345");
 		try { opr.updateOperatoer(oprDTO1); }
 		catch (DALException e) { System.out.println(e.getMessage()); }
-		
+
+		System.out.println("Update Operator 5");
+		oprDTO1.setPassword("hejmeddig");
+		try { opr.updateOperatoer(oprDTO1); }
+		catch (DALException e) { System.out.println(e.getMessage()); }
+
+		System.out.println("Operatoer nummer 5:");
+		try { System.out.println(opr.getOperatoer(5)); }
+		catch (DALException e) { System.out.println(e.getMessage());
+		}
 	}
 }
