d5db3bf9c2e353813711f6babff678e55c5a53d8
Martin <Martin@10.16.230.180>
2016-05-10 14:17:58 +0200

    .

--- Projekt/src/connector01917/Constant.java
+++ Projekt/src/connector01917/Constant.java
@@ -7,7 +7,7 @@ public abstract class Constant
 {
 	public static final String
 		server					= "localhost",  // database-serveren sql-lab1.cc.dtu.dk
-		database				= "Projekt_1",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		database				= "projekt_1",  //"jdbcdatabase", // navnet paa din database = dit studienummer
 		username				= "root", // dit brugernavn = dit studienummer 
 		password				= ""; // dit password som du har valgt til din database
 	
